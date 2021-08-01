e97d4dfaef7770579b557900c3f2d32b27626a21
ваше имя <dimarik-tixonov@yandex.ru>
2021-08-01 20:54:37 +0300

    fix: исправит форму редактирования точки маршрута

--- src/view/edit-point.js
+++ src/view/edit-point.js
@@ -1,4 +1,4 @@
-export const createEditPointForm = () => {
+export const createEditPointForm = () => (
   `<form class="event event--edit" action="#" method="post">
   <header class="event__header">
     <div class="event__type-wrapper">
@@ -156,5 +156,5 @@ export const createEditPointForm = () => {
       <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
     </section>
   </section>
-</form>`;
-};
+</form>`
+);
