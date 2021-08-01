f3dc2274df8637289e6c46fbbcb280e5f68fe31f
ваше имя <dimarik-tixonov@yandex.ru>
2021-08-01 18:44:56 +0300

    feat: добавит функцию отрисовки + модули разметки

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createFiltersTemplate } from './view/filters.js';
+import { createRouteAndPriceTemplate } from './view/route-and-price.js';
+import { createSortingTemplate } from './view/sorting.js';
+import { createTripEventListTemplate } from './view/trip-event-list.js';
+import { createTripEventTemplate } from './view/trip-event.js';
+import { createTripFormCreateTemplate } from './view/trip-form-edit.js';
+import {createMenuTemplate} from './view/trip-mune.js';
+
+const siteHeaderElement = document.querySelector('.page-header');
+const siteTripElement = siteHeaderElement.querySelector('.trip-main');
+const siteNavigationElement = siteHeaderElement.querySelector('.trip-controls__navigation');
+const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const siteMainElement = document.querySelector('.page-main');
+const siteTripEventElement = siteMainElement.querySelector('.trip-events');
+
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+render(siteTripElement, createRouteAndPriceTemplate(), 'afterbegin');
+render(siteNavigationElement, createMenuTemplate(), 'beforeend');
+render(siteFiltersElement, createFiltersTemplate(), 'beforeend');
+render(siteTripEventElement, createSortingTemplate(), 'beforeend');
+render(siteTripEventElement, createTripEventListTemplate(), 'beforeend');
+const siteTripList = siteTripEventElement.querySelector('.trip-events__list');
+render(siteTripList, createTripFormCreateTemplate(), 'beforeend');
+render(siteTripList, createTripEventTemplate(), 'beforeend');
+render(siteTripList, createTripEventTemplate(), 'beforeend');
+render(siteTripList, createTripEventTemplate(), 'beforeend');
